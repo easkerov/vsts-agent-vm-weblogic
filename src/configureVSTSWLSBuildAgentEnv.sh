badfdd557b90a46394ad8dd3bf25e71fc89ca0ae
Emin Askerov <emin@emins-mbp.guest.corp.microsoft.com>
2017-04-24 17:53:23 +0300

    Adding script enhancements

--- src/configureVSTSWLSBuildAgentEnv.sh
+++ src/configureVSTSWLSBuildAgentEnv.sh
@@ -2,32 +2,31 @@
 # This script intended for preparing Oracle Linux/RHEL/CentOS machine to run VSTS private agent and 
 # deploy and build Java EE Apps to Oracle WebLogic Server
 
-cd $HOME
+# Parameters
+vsts_account_name=easkerov
+vsts_personal_access_token=
+vsts_agent_name=vsts-agent-wls
+vsts_agent_pool_name=vsts-wls
+user_account=easkerov
 
-# Downloading all required software components
-echo "Downloading all required software components..."
-# Downloading JDK 8
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm -O "jdk-8-linux-x64.rpm"
-# Downloading Maven
-wget http://www-eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
-# Downloading VSTS Agent
-wget https://github.com/Microsoft/vsts-agent/releases/download/v2.115.0/vsts-agent-rhel.7.2-x64-2.115.0.tar.gz
-# Downloading GIT
-wget https://www.kernel.org/pub/software/scm/git/git-2.9.2.tar.gz
+# Set up variables
+vsts_url=https://$vsts_account_name.visualstudio.com
+agent_url=https://github.com/Microsoft/vsts-agent/releases/download/v2.115.0/vsts-agent-rhel.7.2-x64-2.115.0.tar.gz
+agent_tar=${agent_url##*/}
+agent_home=/opt/vstsagent
 
+jdk_url=http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm 
+jdk_rpm=${jdk_url##*/}
 
-# Unpacking software components
-echo "Unpacking software components..."
-# VSTS Agent
-mkdir vsts_agent && cd vsts_agent
-tar zxvf vsts-agent-rhel.7.2-x64-2.115.0.tar.gz
+maven_url=http://www-eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+maven_tar=${maven_url##*/}
+maven_dir=$(basename $maven_tar .tar.gz)
+maven_home=/usr/local/${maven_dir}
 
-
-# Maven
-sudo tar xzvf apache-maven-3.5.0-bin.tar.gz -C /usr/local
-# GIT
-sudo tar zxfv git-2.9.2.tar.gz -C /usr/src
-
+git_url=https://www.kernel.org/pub/software/scm/git/git-2.9.2.tar.gz
+git_tar=${git_url##*/}
+maven_dir=$(basename $git_tar .tar.gz)
+git_home=/usr/src/${maven_dir}
 
 # Installing all required libs
 echo "Installing required libs..."
@@ -35,22 +34,66 @@ sudo yum -y install libunwind.x86_64 icu
 sudo yum -y install wget deltarpm epel-release unzip libunwind gettext libcurl-devel openssl-devel zlib libicu-devel 
 sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
 sudo yum -y install gcc perl-ExtUtils-MakeMaker
+# Removing the current version of GIT 
+sudo yum -y remove git
+
+# Downloading all required software components
+echo "Downloading all required software components..."
+cd /tmp
+# Downloading JDK 8
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" ${jdk_url}
+# Downloading Maven
+wget ${maven_url}
+# Downloading VSTS Agent
+wget ${agent_url}
+# Downloading GIT
+wget ${git_url}
+
+# Unpacking software components
+echo "Unpacking software components..."
+# Maven
+echo "Maven..."
+sudo mkdir ${maven_home}
+sudo tar -xzf ${maven_tar} --strip-components=1 -C ${maven_home}
+# GIT
+echo "GIT..."
+sudo mkdir ${git_home}
+sudo tar -xzf ${git_tar} --strip-components=1 -C ${git_home}
+# VSTS Agent
+echo "VSTS Agent..."
+sudo mkdir ${agent_home}
+cd ${agent_home}
+sudo tar -xzf /tmp/${agent_tar}
 
 # Installing JDK 8
-sudo rpm -ivh jdk-8-linux-x64.rpm
+sudo rpm -ivh /tmp/${jdk_rpm}
 
 # Installing Git
-cd /usr/src/git-2.9.2
+cd ${git_home}
 sudo make prefix=/usr/local/git all
 sudo make prefix=/usr/local/git install
 
+cd /tmp
 # Creating shell script to set env. variables in /etc/profile.d/
 javapath=$(readlink -f /usr/bin/java | sed "s:/jre/bin/java::")
-cat <<EOF > $HOME/temp/setBuildENV.sh
+cat <<EOF > /tmp/setBuildENV.sh
 export JAVA_HOME=$javapath
-export PATH=$javapath/bin:usr/local/apache-maven-3.5.0/bin:/usr/local/git/bin:\$PATH
+export PATH=\$PATH:$javapath/bin:$maven_home/bin:/usr/local/git/bin
 EOF
 
-sudo chmod a+x $HOME/setBuildENV.sh
-sudo cp $HOME/setBuildENV.sh /etc/profile.d/ 
-source /etc/profile.d/setMVNJDKEnv.sh
+chmod a+x /tmp/setBuildENV.sh
+sudo mv /tmp/setBuildENV.sh /etc/profile.d/ 
+. /etc/profile.d/setBuildENV.sh
+
+export JAVA_HOME=$javapath
+export PATH=$PATH:$javapath/bin:$maven_home/bin:/usr/local/git/bin
+
+# Configure agent
+echo "Running agent configuration"
+sudo -u ${user_account} bash ${agent_home}/config.sh configure --url $vsts_url --agent $vsts_agent_name --pool $vsts_agent_pool_name --nostart --acceptteeeula --auth PAT --token $vsts_personal_access_token --unattended
+
+# Configure agent to run as a service
+sudo bash ${agent_home}/svc.sh install
+sudo bash ${agent_home}/svc.sh start
+
+echo "Done!"
